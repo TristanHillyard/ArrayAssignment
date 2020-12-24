52d21d29a860c7f0d6805507174a3d166ea4f2b5
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-23 18:32:58 -0700

    adding Invalid line. And converting arrays to string form

--- ArrayAssignment/Program.cs
+++ ArrayAssignment/Program.cs
@@ -7,21 +7,47 @@ namespace ArrayAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select an Index from 0-3");
+            Console.WriteLine("Select an Index from 0, 1, 2, 3");
             string[] nameArray = { "John", "Tim", "Chloe", "Kim K" };
             int result = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nameArray[result]);
+            if (result > 4)
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+            else
+            {
+                Console.WriteLine(nameArray[result]);
+            }
 
-            string inputNum = Console.ReadLine();
 
-            Console.WriteLine("Select an index from 0-4");
+            Console.WriteLine("Select an index from 0, 1, 2, 3, 4");
             int[] intArray = { 2, 10, 20, 30, 50 };
+            int numResult = Convert.ToInt32(Console.ReadLine());
+            if(numResult > 5)
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+            else
+            {
+                Console.WriteLine(intArray[numResult]);
+            }
+            
 
-            Console.WriteLine("Select an Index of 0-2");
+            Console.WriteLine("Select an Index of 0, 1, or 2");
             List<string> strgList = new List<string>();
             strgList.Add("Hello");
             strgList.Add("Crazy");
             strgList.Add("Mello");
+            int strgResult = Convert.ToInt32(Console.ReadLine());
+            if(strgResult > 3)
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+            else
+            {
+                Console.WriteLine(strgList[strgResult]);
+            }
+
         }
     }
 }
